c88e80f2e4ec5e57fd765a5af85385acd7d54d8b
oyeyinkaojora <oyeyinkaojoro@gmail.com>
2022-05-23 23:56:15 +0100

    made little changes

--- src/Box.jsx
+++ src/Box.jsx
@@ -4,7 +4,7 @@ import Icon from "./assets/img/icon-dice.svg";
 
 const Box = ({ qoutes, onclick }) => {
   return (
-    <div className="p-6 font-container relative justify-center mt-60 w-4/5 bg-darkGrayishBlue mx-auto flex flex-col items-center rounded-xl  md:w-2/6">
+    <div role="main" className="p-6 font-container relative justify-center mt-60 w-4/5 bg-darkGrayishBlue mx-auto flex flex-col items-center rounded-xl  md:w-2/6">
       <div>
         <h1 className="text-neonGreen text-center tracking-wide mb-3">
           A D V I C E #{qoutes?.id}
@@ -16,7 +16,7 @@ const Box = ({ qoutes, onclick }) => {
       <img src={Pattern} alt="pattern" className="mt-2 hidden md:flex " />
       <img src={PatternMobile} alt="pattern " className="mt-2 flex md:hidden" />
       <div className="rounded-full p-5 mt-5 -mb-14 bg-neonGreen diceIcon">
-        <img onClick={onclick} src={Icon} alt="icon" />
+        <img onClick={onclick}  src={Icon} alt="icon" />
       </div>
     </div>
   );
